1dafe4b0b969de9aeb19a3d6d266e746b2399b20
Bin Zhang <sjtuzb@gmail.com>
2015-05-31 10:39:36 +0800

    complete the model

--- restless_api/model.py
+++ restless_api/model.py
@@ -25,10 +25,10 @@ db = SQLAlchemy(app)
 class User(db.Model):
     __tablename__ = 'user'
 
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(255), unique=True)
-    password = db.Column(db.String(255))
-    email = db.Column(db.String(255), unique=True)
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    username = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
 
     def __init__(self, username, password, email):
         self.username = username
@@ -39,58 +39,63 @@ class User(db.Model):
 class Building(db.Model):
     __tablename__ = 'building'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.String(255), nullable=False)
     latitude = db.Column(db.String(255))
     longitude = db.Column(db.String(255))
     description = db.Column(db.String(255))
+    relationship = db.relationship('Floor', backref='building', lazy='dynamic')
 
 
 class Floor(db.Model):
     __tablename__ = 'floor'
 
-    id = db.Column(db.Integer, primary_key=True)
-    building_id = db.Column(db.Integer)
-    name = db.Column(db.String(255))
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    building_id = db.Column(db.Integer, db.ForeignKey('building.id'), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
     description = db.Column(db.String(255))
+    relationship = db.relationship('Room', backref='floor', lazy='dynamic')
 
 
 class Room(db.Model):
     __tablename__ = 'room'
 
-    id = db.Column(db.Integer, primary_key=True)
-    floor_id = db.Column(db.Integer)
-    name = db.Column(db.String(255))
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    floor_id = db.Column(db.Integer, db.ForeignKey('floor.id'), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
     description = db.Column(db.String(255))
+    relationship = db.relationship('Device', backref='room', lazy='dynamic')
 
 
 class Device(db.Model):
     __tablename__ = 'device'
 
-    id = db.Column(db.Integer, primary_key=True)
-    room_id = db.Column(db.Integer)
-    name = db.Column(db.String(255))
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    room_id = db.Column(db.Integer, db.ForeignKey('room.id'), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
     description = db.Column(db.String(255))
+    relationship = db.relationship('SensorData', backref='device', lazy='dynamic')
 
 
 class Sensor(db.Model):
     __tablename__ = 'sensor'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
-    type = db.Column(db.String(255))
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    type = db.Column(db.String(255), nullable=False)
     description = db.Column(db.String(255))
+    relationship = db.relationship('SensorData', backref='sensor', lazy='dynamic')
 
 
 class SensorData(db.Model):
     __tablename__ = 'sensor_data'
 
-    id = db.Column(db.Integer, primary_key=True)
-    sensor_id = db.Column(db.Integer)
-    device_id = db.Column(db.Integer)
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    sensor_id = db.Column(db.Integer, db.ForeignKey('sensor.id'), nullable=False)
+    device_id = db.Column(db.Integer, db.ForeignKey('device.id'), nullable=False)
     value = db.Column(db.Float)
     datetime = db.Column(db.DateTime)
     status = db.Column(db.Integer)
 
 # Create the database tables.
-# db.create_all()
+#db.create_all()
